b231e6f2378328ea0ee27193dddfced5d24f8a84
Ioannes <5526173+itrukhin@users.noreply.github.com>
2023-07-22 06:09:33 +0300

    checkModules method

--- components/core/class.php
+++ components/core/class.php
@@ -3,11 +3,20 @@ declare(strict_types=1);
 namespace App\Components;
 
 use App\BxKint;
+use Bitrix\Main\Loader;
+use Bitrix\Main\LoaderException;
 
 class CoreComponent extends \CBitrixComponent {
 
+    protected function checkModules()
+    {
+        if (!Loader::includeModule('dvk.feature')) {
+            throw new LoaderException('dvk.feature module not installed!');
+        }
+    }
     public function executeComponent()
     {
+        $this->checkModules();
         $this->includeComponentTemplate();
 
         if(class_exists('\App\BxKint'))
