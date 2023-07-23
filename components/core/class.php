78c6b96ed6457206101ea184774c4c7ff336ec47
Ioannes <5526173+itrukhin@users.noreply.github.com>
2023-07-23 03:41:38 +0300

    component processRequest

--- components/core/class.php
+++ components/core/class.php
@@ -8,19 +8,43 @@ use Bitrix\Main\LoaderException;
 
 class CoreComponent extends \CBitrixComponent {
 
-    protected function checkModules()
-    {
-        if (!Loader::includeModule('dvk.feature')) {
-            throw new LoaderException('dvk.feature module not installed!');
-        }
-    }
     public function executeComponent()
     {
         $this->checkModules();
+        $this->processRequest();
+
         $this->includeComponentTemplate();
 
         if(class_exists('\App\BxKint'))
             BxKint::info($this->arResult);
         return $this->arResult;
+    }
+
+    protected function processRequest(): bool
+    {
+        if($this->request->isPost()) {
+            $postData = $this->request->getPostList()->toArray();
+            return true;
+        }
+        return false;
+    }
+
+    public function onPrepareComponentParams($params)
+    {
+        $params = parent::onPrepareComponentParams($params);
+
+        if (!isset($params["CACHE_TIME"])) {
+            $params["CACHE_TIME"] = 86400;
+        }
+        $params['CACHE_GROUPS'] = ($params['CACHE_GROUPS'] == 'Y');
+
+        return $params;
+    }
+
+    protected function checkModules()
+    {
+        if (!Loader::includeModule('dvk.feature')) {
+            throw new LoaderException('dvk.feature module not installed!');
+        }
     }
 }
